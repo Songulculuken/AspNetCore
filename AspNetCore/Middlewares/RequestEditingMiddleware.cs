fc2988b2d5c873e08c8fd95c3240083b1a94b46c
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-08-07 14:23:00 +0300

    Add project files.

---
+++ AspNetCore/Middlewares/RequestEditingMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Middlewares
+{
+    public class RequestEditingMiddleware
+    {
+        private RequestDelegate _requestDelegate;
+        public RequestEditingMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;     
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            ///ysk.com.tr/songül slaş songül path dir
+            if (context.Request.Path.ToString() == "/songul")
+                await context.Response.WriteAsync("hosgeldin songul");
+            else
+                await _requestDelegate.Invoke(context);   //asenktronik olduğu için await. Clientden gelen istek middleware e gider.  
+        }
+    }
+}
