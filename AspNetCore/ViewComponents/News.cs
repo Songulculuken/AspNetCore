3b322681b4eb911c1945b0000926c0384cb4acda
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-08-11 15:24:07 +0300

    Parametre gönderimi view component

--- AspNetCore/ViewComponents/News.cs
+++ AspNetCore/ViewComponents/News.cs
@@ -5,10 +5,19 @@ namespace AspNetCore.ViewComponents
 {
     public class News : ViewComponent //askenronik olarak çağırırız
     {
-        public IViewComponentResult Invoke() //Invoke methodu ile ayağa kaldırırız.
+        public IViewComponentResult Invoke(string color = "default") //Invoke methodu ile ayağa kaldırırız. string color ="default" parametre gönderimi
         {
             var list = NewsContext.List;
-            return View(list);  
+            if (color == "default")
+            {
+                return View(list);
+            }
+            else if (color == "red")
+            {
+                return View("red", list);
+            }
+            else
+                return View("blue", list);
         }
     }
 }
