1d14c5069fc79d1f15eb773c7c51a296d63ba318
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-08-11 15:18:26 +0300

    View Component

---
+++ AspNetCore/ViewComponents/News.cs
@@ -0,0 +1,14 @@
+﻿using AspNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.ViewComponents
+{
+    public class News : ViewComponent //askenronik olarak çağırırız
+    {
+        public IViewComponentResult Invoke() //Invoke methodu ile ayağa kaldırırız.
+        {
+            var list = NewsContext.List;
+            return View(list);  
+        }
+    }
+}
