2968627d568b122633f41616a1077e6f20660929
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-08-11 11:10:12 +0300

    DirectoryInfo classı ile folderlist

---
+++ AspNetCore/Controllers/FolderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace AspNetCore.Controllers
+{
+    public class FolderController: Controller
+    {
+        public IActionResult List() 
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")); //classtan yardım alarak klasördekileri listeleme
+            var folders= directoryInfo.GetDirectories();    
+            return View(folders);  
+        }
+    }
+}
