6c80b222480c091a9b70824a7d7916162eb24582
Songül ÇULUKEN <songulculuken97@gmail.com>
2023-08-11 11:40:20 +0300

    Folder List And TempData Error Message

--- AspNetCore/Controllers/FolderController.cs
+++ AspNetCore/Controllers/FolderController.cs
@@ -8,8 +8,42 @@ namespace AspNetCore.Controllers
         public IActionResult List() 
         {
             DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")); //classtan yardım alarak klasördekileri listeleme
-            var folders= directoryInfo.GetDirectories();    
-            return View(folders);  
+            var folders= directoryInfo.GetDirectories();
+            return View(folders);
+        }
+        public IActionResult Create()
+        {
+            if (TempData.ContainsKey("ErrorMessage"))
+            {
+                ViewBag.ErrorMessage = TempData["ErrorMessage"];
+                ViewBag.FolderName = TempData["FolderName"];
+            }
+            return View();  
+        }
+        [HttpPost]
+        public IActionResult Create(string folderName)
+        {
+            DirectoryInfo info = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot",folderName));
+            if(!info.Exists)
+            {
+                info.Create();
+                return RedirectToAction("List");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Bu klasör zaten mevcut.";
+                TempData["FolderName"] = folderName;
+                return RedirectToAction("Create");
+            }
+        }
+        public IActionResult Remove(string folderName)
+        {
+            DirectoryInfo info = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", folderName));
+            if(info.Exists) 
+            {
+                info.Delete(true);
+            }
+            return RedirectToAction("List");
         }
     }
 }
